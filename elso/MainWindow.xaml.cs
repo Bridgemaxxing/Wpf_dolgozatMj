123f4f7f6d5c842754ba1ba7fc496204ef7964bc
Bridgemaxxing <marthajozsef2@gmail.com>
2025-05-09 12:50:50 +0200

    Add project files.

---
+++ elso/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace elso
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_szamol_Click(object sender, RoutedEventArgs e)
+        {
+            // int szam1;
+            //int szam1 = int.TryParse(tbx_szam1, out szam1);
+
+            if (double.TryParse(tbx_szam1.Text, out double szam1) && double.TryParse(tbx_szam2.Text, out double szam2))
+            {
+                double megoldas = (szam1 + szam2) / 2;
+                tbx_szamtanikoz.Text = megoldas.ToString();
+                double megoldas2 = Math.Sqrt(szam1 * szam2);
+                tbx_mertanikoz.Text = megoldas2.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Hiba! Nem megfelelő bevitel\nSzámnak kell lennie!", "Hiba", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+
+            
+        }
+
+       
+    }
+}
---
+++ masodik/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace masodik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_szamol_Click(object sender, RoutedEventArgs e)
+        {
+            double eredmeny = 0;
+            if (double.TryParse(tbx_szam1.Text, out double szam1) && double.TryParse(tbx_szam2.Text, out double szam2))
+            {
+                switch (tbx_muveletiJel.Text)
+                {
+                    case "+":
+                        eredmeny = szam1 + szam2;
+                        tbx_eredmeny.Text = eredmeny.ToString();
+                        break;
+                    case "-":
+                        eredmeny = szam1 - szam2;
+                        tbx_eredmeny.Text = eredmeny.ToString();
+                        break;
+                    case "*":
+                        eredmeny = szam1 * szam2;
+                        tbx_eredmeny.Text = eredmeny.ToString();
+                        break;
+                    case "/":
+                        if (szam1 <= 0)
+                        {
+                            MessageBox.Show("Hiba! Az első számnak nagyobbnak kell lennie mint 0", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                            tbx_szam1.Clear();
+                            tbx_szam2.Clear();
+                        }
+                        else
+                        {
+                            eredmeny = szam1 / szam2;
+                            tbx_eredmeny.Text = eredmeny.ToString();
+                            break;
+                        }
+                        
+                        break ;
+                    default:
+                        MessageBox.Show("Hiba! Nem megfelelő műveleti jel!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                        tbx_muveletiJel.Text = "";
+                        break;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hiba! Nem számot attál meg!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                tbx_szam1.Clear();
+                tbx_szam2.Clear();
+                tbx_muveletiJel.Clear();
+            }
+        }
+    }
+}
